5b1e025641d165c7a192bdb6e58d88a226108ade
lucmarty <lucasmarty133@gmail.com>
2026-09-18 16:35:22 +0200

    Exercice5

--- src/test/java/ValideurEmailTest.java
+++ src/test/java/ValideurEmailTest.java
@@ -1,5 +1,7 @@
 import Exercice2.ValidateurEmail;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,5 +27,17 @@ public class ValideurEmailTest {
     @Test
     void testBonneSyntax() {
         assertTrue(ValidateurEmail.estValide("le@email.com"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"aaaa@bbbb.com", "bbbb@.", "@."})
+    void testValideMails(String mails) {
+        assertTrue(ValidateurEmail.estValide(mails));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"aaaabbbb.com", "bbbb@", ""})
+    void testNonValideMails(String mails) {
+        assertFalse(ValidateurEmail.estValide(mails));
     }
 }
